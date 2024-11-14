45b7af306ead0292ebfb6f931f83edfee54b9e07
vvvbbbcz <vvvbbbcz@bzgzs.cn>
2024-11-14 23:30:37 +0800

    feat: article list interface

---
+++ components/AdminArticleDetail.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps(['refresh', 'row', 'column', '$index']);
+</script>
+
+<template>
+	<span>标签：</span>
+	<el-tag type="primary" v-for="tag in props.row.tagId">
+		{{ tag.name }}
+	</el-tag>
+</template>
+
+<style scoped>
+.el-tag {
+	margin-left: 1rem;
+}
+</style>
--- components/AdminArticleList.vue
+++ components/AdminArticleList.vue
@@ -1,63 +1,69 @@
 <script setup lang="ts">
+
 const props = defineProps({
 	draft: Boolean
 });
 
 const type = props.draft ? 'draft' : 'article';
-const {data} = await useFetch(`/api/admin/${type}/list`);
-const articles = data.value;
+const {data: articles} = await useFetch(`/api/admin/${type}/list`);
 
 async function edit(id: number) {
 	await navigateTo(`/admin/${type}/edit/${id}`);
 }
+
+const mounted = ref<boolean>(false);
+onMounted(() => {
+	mounted.value = true;
+});
 </script>
 
 <template>
 	<el-card>
-		<div class="list-item" v-for="article in articles">
-			<el-container direction="horizontal">
-				<div>
-					<h1>{{ article.title }}</h1>
-					<el-tag v-if="article.visible" type="primary">公开</el-tag>
+		<el-table v-if="mounted" :data="articles">
+			<el-table-column type="expand">
+				<template #default="props">
+					<div class="table-expand">
+						<AdminArticleDetail :row="props.row" :column="props.column" :$index="props.$index"/>
+					</div>
+				</template>
+			</el-table-column>
+			<el-table-column prop="title" label="标题" min-width="150">
+				<template #default="props">
+					<h1>{{ props.row.title }}</h1>
+				</template>
+			</el-table-column>
+			<el-table-column prop="author" label="作者" min-width="100">
+				<template #default="props">
+					{{ props.row.author.nickname }}
+				</template>
+			</el-table-column>
+			<el-table-column v-if="!props.draft" prop="publishDate" label="发布时间" min-width="100">
+			</el-table-column>
+			<el-table-column v-if="!props.draft" prop="updateDate" label="更新时间" min-width="100">
+			</el-table-column>
+			<el-table-column prop="visible" label="可见性" min-width="70">
+				<template #default="props">
+					<el-tag v-if="props.row.visible" type="primary">公开</el-tag>
 					<el-tag v-else type="info">私密</el-tag>
-				</div>
-				<div>
-					<el-button-group>
-						<el-button>
-							Detail
-						</el-button>
-						<el-button type="primary" @click="edit(article._id)">
-							编辑
-						</el-button>
-					</el-button-group>
-				</div>
-			</el-container>
-			{{ article }}
-		</div>
+				</template>
+			</el-table-column>
+			<el-table-column fixed="right" label="操作" width="85">
+				<template #default="props">
+					<el-button type="primary" @click="edit(props.row._id)">
+						编辑
+					</el-button>
+				</template>
+			</el-table-column>
+		</el-table>
 	</el-card>
 </template>
 
 <style scoped>
 h1 {
-	margin: 0 0.5rem;
-	display: inline-block;
+	margin: 0;
 }
 
-.list-item {
-	border-bottom: solid 1px lightgray;
-}
-
-.list-item:first-child {
-	border-top: solid 1px lightgray;
-}
-
-.el-container {
-	margin: 0.5rem 0;
-	align-items: center;
-	justify-content: space-between;
-}
-
-.el-button-group {
-	margin-right: 0.5rem;
+.table-expand {
+	margin-left: 2rem;
 }
 </style>
