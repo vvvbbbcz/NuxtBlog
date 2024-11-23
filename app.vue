2f6df1c928e0e84f47f1bd3987ed1a43059d6539
vvvbbbcz <vvvbbbcz@bzgzs.cn>
2024-11-23 20:12:56 +0800

    style: code order of app.vue

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+const {data} = await useAsyncData('blogInfo', () => $fetch('/api/getBlogInfo'));
+const title = data.value ? data.value.name : '获取标题失败';
+useHead({
+	title: `${title}`,
+});
+</script>
+
 <template>
 	<NuxtLayout>
 		<NuxtPage/>
@@ -11,10 +19,3 @@ body {
 	margin: 0;
 }
 </style>
-<script setup lang="ts">
-const {data} = await useAsyncData('blogInfo', () => $fetch('/api/getBlogInfo'));
-const title = data.value ? data.value.name : '获取标题失败';
-useHead({
-	title: `${title}`,
-});
-</script>
