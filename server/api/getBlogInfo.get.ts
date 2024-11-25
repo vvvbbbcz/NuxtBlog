b29273eee9e5156b29a99f348510518e9d1fd4e3
vvvbbbcz <vvvbbbcz@bzgzs.cn>
2024-11-25 23:26:18 +0800

    feat: db connection check before find blog info

--- server/api/getBlogInfo.get.ts
+++ server/api/getBlogInfo.get.ts
@@ -1,12 +1,14 @@
 import BlogInfo from '~/server/utils/models/BlogInfo';
+import mongoose from "mongoose";
 
 export default defineEventHandler(async (event) => {
-	const data = await BlogInfo.findOne()
-		.select(['-_id', 'name', 'icon', 'separator', 'background'])
-		.lean();
-	if (data) {
-		return data;
-	} else {
-		return apiStatus.error(event, 500);
+	if (mongoose.connection.readyState) {
+		const data = await BlogInfo.findOne()
+			.select(['-_id', 'name', 'icon', 'separator', 'background'])
+			.lean();
+		if (data) {
+			return data;
+		}
 	}
+	return apiStatus.error(event, 500);
 })
