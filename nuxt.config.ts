7e0fcd1572e795906ff376b30ae887d226b4a3b6
vvvbbbcz <vvvbbbcz@bzgzs.cn>
2024-11-25 13:37:38 +0800

    chore: remove timeline in dev tools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,6 @@ export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
 	devtools: {
 		enabled: true,
-
-		timeline: {
-			enabled: true
-		}
 	},
 	modules: [
 		'@element-plus/nuxt',
