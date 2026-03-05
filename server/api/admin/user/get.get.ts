8239a73b7592224db379f2c08593483cef165a03
vvvbbbcz <ms_vvvbbbcz@mail.scut.edu.cn>
2026-03-05 22:53:29 +0800

    refactor: id filter for admin user api

--- server/api/admin/user/get.get.ts
+++ server/api/admin/user/get.get.ts
@@ -1,23 +1,24 @@
 import User from "~/server/utils/models/BlogData";
+import filters from "~/server/utils/filters";
 
 export default defineEventHandler(async (event) => {
-	const query = getQuery(event).id;
-	const id = parseInt(Array.isArray(query) ? query[0] : query);
-	if (!isNaN(id) && id >= -128 && id < 0) {
-		const { user }: any = await getUserSession(event);
-		if (user._id !== id) {
-			throw createError({ statusCode: 404, statusMessage: 'User Not Found' });
-		}
+    const query = getQuery(event).id;
+    const id = parseInt(Array.isArray(query) ? query[0] : query);
+    if (filters.isUser(id)) {
+        const { user }: any = await getUserSession(event);
+        if (user._id !== id) {
+            throw createError({ statusCode: 404, statusMessage: 'User Not Found' });
+        }
 
-		const data = await User
-			.findById(id)
-			.select(['-_id', 'ur', 'ti', 'md', 'co'])
-			.lean();
-		if (data) {
-			return data;
-		} else {
-			throw createError({ statusCode: 404, statusMessage: 'User Not Found' });
-		}
-	}
-	throw createError({ statusCode: 400, statusMessage: 'Invalid ID' });
+        const data = await User
+            .findById(id)
+            .select(['-_id', 'ur', 'ti', 'md', 'co'])
+            .lean();
+        if (data) {
+            return data;
+        } else {
+            throw createError({ statusCode: 404, statusMessage: 'User Not Found' });
+        }
+    }
+    throw createError({ statusCode: 400, statusMessage: 'Invalid ID' });
 });
