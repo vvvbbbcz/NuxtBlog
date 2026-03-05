d0dd03d829c3c4057f818fa4b70a3132e8d53b60
vvvbbbcz <ms_vvvbbbcz@mail.scut.edu.cn>
2026-03-05 22:50:54 +0800

    refactor: add id filter

--- server/utils/filters.ts
+++ server/utils/filters.ts
@@ -1,13 +1,26 @@
+const blogInfoId = 0;
+const user_menuItem = -128;
+const menuItem_tag = -256;
+const tag_picture = -8192;
+
 export default {
-	article: {
-		all: function (additional?: any) { return { _id: { $gt: 0 }, ...additional } },
-		published: function (additional?: any) { return { _id: { $gt: 0 }, dr: false, de: false, ...additional } },
-		updated: function (additional?: any) { return { _id: { $gt: 0 }, dr: false, de: false, ...additional } },
-		drafted: function (additional?: any) { return { _id: { $gt: 0 }, dr: true, de: false, ...additional } },
-		deleted: function (additional?: any) { return { _id: { $gt: 0 }, de: true, ...additional } },
-	},
-	blog_info: function (additional?: any) { return { _id: 0, ...additional } },
-	user: function (additional?: any) { return { _id: { $gte: -128, $lt: 0 }, ...additional } },
-	menuItem: function (additional?: any) { return { _id: { $gte: -256, $lt: -128 }, ...additional } },
-	tag: function (additional?: any) { return { _id: { $gte: -8192, $lt: -256 }, ...additional } },
+    article: {
+        all: function (additional?: any) { return { _id: { $gt: blogInfoId }, ...additional } },
+        published: function (additional?: any) { return { _id: { $gt: blogInfoId }, dr: false, de: false, ...additional } },
+        updated: function (additional?: any) { return { _id: { $gt: blogInfoId }, dr: false, de: false, ...additional } },
+        drafted: function (additional?: any) { return { _id: { $gt: blogInfoId }, dr: true, de: false, ...additional } },
+        deleted: function (additional?: any) { return { _id: { $gt: blogInfoId }, de: true, ...additional } },
+    },
+    blog_info: function (additional?: any) { return { _id: blogInfoId, ...additional } },
+    user: function (additional?: any) { return { _id: { $gte: user_menuItem, $lt: blogInfoId }, ...additional } },
+    menuItem: function (additional?: any) { return { _id: { $gte: menuItem_tag, $lt: user_menuItem }, ...additional } },
+    tag: function (additional?: any) { return { _id: { $gte: tag_picture, $lt: menuItem_tag }, ...additional } },
+    picture: function (additional?: any) { return { _id: { $lt: tag_picture }, ...additional } },
+
+    isArticle: function (id: number) { return id > blogInfoId },
+    isBlogInfo: function (id: number) { return id === blogInfoId },
+    isUser: function (id: number) { return id >= user_menuItem && id < blogInfoId },
+    isMenuItem: function (id: number) { return id >= menuItem_tag && id < user_menuItem },
+    isTag: function (id: number) { return id >= tag_picture && id < menuItem_tag },
+    isPicture: function (id: number) { return id < tag_picture }
 }
