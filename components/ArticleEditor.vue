c6370d17c4ea251c4d63fb2057625ccb17689971
vvvbbbcz <ms_vvvbbbcz@mail.scut.edu.cn>
2026-03-06 21:27:51 +0800

    chore: change indent

--- components/ArticleEditor.vue
+++ components/ArticleEditor.vue
@@ -7,103 +7,103 @@ import { aesEncrypt, generateIV } from "~/utils/aesCrypto";
 import type { Article } from "~/utils/dbTypes/article";
 
 const props = defineProps({
-	id: Number,
+    id: Number,
 });
 
 const article = ref<Article>({ visible: 0 });
 
 if (props.id !== undefined) {
-	const { data, status, error } =
-		await useFetch('/api/admin/article/get', { query: { id: props.id } });
+    const { data, status, error } =
+        await useFetch('/api/admin/article/get', { query: { id: props.id } });
 
-	if (data.value) {
-		article.value = data.value;
-	} else if (status.value === 'error') {
-		ElNotification({ type: 'error', title: '获取文章失败', message: error.value?.message });
-	}
+    if (data.value) {
+        article.value = data.value;
+    } else if (status.value === 'error') {
+        ElNotification({ type: 'error', title: '获取文章失败', message: error.value?.message });
+    }
 }
 
 const unsaved = ref<boolean>(false);
 const form = ref<InstanceType<typeof ArticleInfoForm>>();
 
 async function update(draft: boolean) {
-	if (await form.value?.validate()) {
-		const { user }: any = useUserSession();
+    if (await form.value?.validate()) {
+        const { user }: any = useUserSession();
 
-		if (!draft) {
-			if (article.value.visible === 2) {
-				const iv = generateIV();
-				article.value.iv = Array.from(iv);
-				article.value.html = await aesEncrypt(article.value.password || "", iv, vditor.getHTML());
-			} else {
-				article.value.iv = undefined;
-				article.value.html = vditor.getHTML();
-			}
-		} else {
-			article.value.iv = undefined;
-			article.value.html = undefined;
-		}
+        if (!draft) {
+            if (article.value.visible === 2) {
+                const iv = generateIV();
+                article.value.iv = Array.from(iv);
+                article.value.html = await aesEncrypt(article.value.password || "", iv, vditor.getHTML());
+            } else {
+                article.value.iv = undefined;
+                article.value.html = vditor.getHTML();
+            }
+        } else {
+            article.value.iv = undefined;
+            article.value.html = undefined;
+        }
 
-		article.value.markdown = vditor.getValue();
-		article.value.date = moment().format("YYYY-MM-DD HH:mm:ss");
-		article.value.year = moment().year();
-		article.value.author = user.value._id; // TODO
-		article.value.drafted = draft;
+        article.value.markdown = vditor.getValue();
+        article.value.date = moment().format("YYYY-MM-DD HH:mm:ss");
+        article.value.year = moment().year();
+        article.value.author = user.value._id; // TODO
+        article.value.drafted = draft;
 
-		const { data, status }: any = await $fetch(`/api/admin/article/${props.id ? 'update' : 'create'}`, {
-			method: props.id ? 'PATCH' : 'POST',
-			body: article.value
-		}).catch(error => {
-			ElNotification({ type: 'error', title: '保存失败', message: error });
-		});
-		if (status === 'success') {
-			ElMessage({ type: 'success', message: '保存成功' });
-			unsaved.value = false;
-			if (!props.id) { // create
-				await navigateTo(`/admin/article/edit/${data.id}`);
-			}
-		}
-	}
+        const { data, status }: any = await $fetch(`/api/admin/article/${props.id ? 'update' : 'create'}`, {
+            method: props.id ? 'PATCH' : 'POST',
+            body: article.value
+        }).catch(error => {
+            ElNotification({ type: 'error', title: '保存失败', message: error });
+        });
+        if (status === 'success') {
+            ElMessage({ type: 'success', message: '保存成功' });
+            unsaved.value = false;
+            if (!props.id) { // create
+                await navigateTo(`/admin/article/edit/${data.id}`);
+            }
+        }
+    }
 }
 
 let vditor: Vditor;
 onMounted(async () => {
-	vditor = new Vditor('vditor', {
-		height: '100%',
-		value: article.value.markdown,
-		toolbarConfig: {
-			pin: true,
-		},
-		input(value) {
-			unsaved.value = true;
-		},
-		cache: {
-			enable: false,
-		},
-		cdn: "/vditor"
-	});
+    vditor = new Vditor('vditor', {
+        height: '100%',
+        value: article.value.markdown,
+        toolbarConfig: {
+            pin: true,
+        },
+        input(value) {
+            unsaved.value = true;
+        },
+        cache: {
+            enable: false,
+        },
+        cdn: "/vditor"
+    });
 })
 </script>
 
 <template>
-	<el-container class="h-100p" direction="vertical">
-		<ArticleInfoForm ref="form" :info="article" @change="unsaved = true" />
-		<div class="m-b-1 d-fl a-i-c">
-			<el-button-group class="m-r-1">
-				<el-button v-if="article.drafted" type="primary" @click="update(true)">
-					保存草稿
-				</el-button>
-				<el-button v-else type="primary" @click="update(true)">
-					转为草稿
-				</el-button>
-				<el-button type="primary" @click="update(false)">
-					发布
-				</el-button>
-			</el-button-group>
-			<el-text v-if="unsaved" class="m-l-1" type="warning">未保存</el-text>
-		</div>
-		<div id="vditor"></div>
-	</el-container>
+    <el-container class="h-100p" direction="vertical">
+        <ArticleInfoForm ref="form" :info="article" @change="unsaved = true" />
+        <div class="m-b-1 d-fl a-i-c">
+            <el-button-group class="m-r-1">
+                <el-button v-if="article.drafted" type="primary" @click="update(true)">
+                    保存草稿
+                </el-button>
+                <el-button v-else type="primary" @click="update(true)">
+                    转为草稿
+                </el-button>
+                <el-button type="primary" @click="update(false)">
+                    发布
+                </el-button>
+            </el-button-group>
+            <el-text v-if="unsaved" class="m-l-1" type="warning">未保存</el-text>
+        </div>
+        <div id="vditor"></div>
+    </el-container>
 </template>
 
 <style scoped></style>
--- components/ArticleInfoForm.vue
+++ components/ArticleInfoForm.vue
@@ -8,117 +8,117 @@ const undef = undefined;
 
 const form = ref<FormInstance>();
 const rule = ref<FormRules<Article>>({
-	url: [{ required: true, message: '请输入 URL 标题', trigger: 'blur' }],
-	title: [{ required: true, message: '请输入标题', trigger: 'blur' }],
-	password: [{ required: true, validator: validatePassword, trigger: 'blur' }],
+    url: [{ required: true, message: '请输入 URL 标题', trigger: 'blur' }],
+    title: [{ required: true, message: '请输入标题', trigger: 'blur' }],
+    password: [{ required: true, validator: validatePassword, trigger: 'blur' }],
 });
 
 function validatePassword(rule: any, value: any, callback: any) {
-	if (props.info.visible === 2 && !value) { // strength bigger than 0
-		callback(new Error('请输入密码'))
-	} else {
-		callback()
-	}
+    if (props.info.visible === 2 && !value) { // strength bigger than 0
+        callback(new Error('请输入密码'))
+    } else {
+        callback()
+    }
 }
 
 async function validate(callback?: FormValidateCallback): Promise<boolean> {
-	return form.value?.validate(callback) || false;
+    return form.value?.validate(callback) || false;
 }
 
 defineExpose({ validate })
 
 const visibleOptions = [
-	{ value: 0, label: '公开' },
-	{ value: 1, label: '私密' },
-	{ value: 2, label: '加密' }
+    { value: 0, label: '公开' },
+    { value: 1, label: '私密' },
+    { value: 2, label: '加密' }
 ]
 
 
 const tagList = ref<any[]>([]);
 const { data: tagListData, execute: startFetchTags, status: tagsStatus } =
-	await useLazyFetch('/api/admin/tag/editorList', { key: 'tagList', immediate: false });
+    await useLazyFetch('/api/admin/tag/editorList', { key: 'tagList', immediate: false });
 
 async function fetchTags() {
-	const { data: tags } = useNuxtData('tagList');
-	if (tags.value) {
-		tagList.value = tags.value;
-	} else {
-		await startFetchTags();
-		if (tagsStatus.value === 'success') {
-			tagList.value = tagListData.value || [];
-		} else if (tagsStatus.value === 'error') {
-			ElMessage({ type: 'error', message: '获取标签失败' });
-		}
-	}
+    const { data: tags } = useNuxtData('tagList');
+    if (tags.value) {
+        tagList.value = tags.value;
+    } else {
+        await startFetchTags();
+        if (tagsStatus.value === 'success') {
+            tagList.value = tagListData.value || [];
+        } else if (tagsStatus.value === 'error') {
+            ElMessage({ type: 'error', message: '获取标签失败' });
+        }
+    }
 }
 
 const newTagName = ref<string>('')
 
 async function confirmNewTag() {
-	if (newTagName.value) {
-		// useFetch('/api/addTag') // TODO
-		newTagName.value = '';
-	}
+    if (newTagName.value) {
+        // useFetch('/api/addTag') // TODO
+        newTagName.value = '';
+    }
 }
 
 const mounted = ref(false);
 onMounted(async () => {
-	mounted.value = true;
+    mounted.value = true;
 });
 </script>
 
 <template>
-	<el-form v-if="mounted" ref="form" :model="props.info" :rules="rule" label-width="auto" hide-required-asterisk
-		status-icon>
-		<el-form-item prop="ti" label="标题">
-			<el-input v-model="props.info.title" @change="$emit('change')" />
-		</el-form-item>
-		<el-form-item prop="ur" label="URL">
-			<el-input v-model="props.info.url" @change="$emit('change')" />
-		</el-form-item>
-		<el-form-item prop="tg" label="标签">
-			<el-select v-model="props.info.tag" multiple filterable remote collapse-tags collapse-tags-tooltip
-				:max-collapse-tags="5" remote-show-suffix :remote-method="fetchTags" :loading="tagsStatus === 'pending'"
-				@change="$emit('change')">
-				<el-option v-for="item in tagList" :label="item.ti || ''" :value="item._id" />
-				<template #loading>
-					<svg class="circular" viewBox="0 0 50 50">
-						<circle class="path" cx="25" cy="25" r="20" fill="none" />
-					</svg>
-				</template>
-				<template #footer>
-					<el-input v-model="newTagName" class="w-100p m-b-_5" placeholder="添加标签（暂不支持）" />
-					<el-button type="primary" @click="confirmNewTag">
-						添加
-					</el-button>
-					<el-button @click="newTagName = ''">
-						取消
-					</el-button>
-				</template>
-			</el-select>
-		</el-form-item>
-		<el-form-item prop="visible" label="可见性">
-			<div style="display: flex;">
-				<el-segmented v-if="props.info.visible !== null" v-model="props.info.visible" :options="visibleOptions">
-					<template #default="{ item }">
-						<div class="flex flex-col items-center gap-2 p-2">
-							<div>{{ item.label }}</div>
-						</div>
-					</template>
-				</el-segmented>
-				<el-segmented v-else v-model="undef" :options="visibleOptions">
-					<template #default="{ item }">
-						<div class="flex flex-col items-center gap-2 p-2">
-							<div>{{ item.label }}</div>
-						</div>
-					</template>
-				</el-segmented>
-				<el-form-item prop="pw" label="密码" v-if="props.info.visible === 2">
-					<el-input v-model="props.info.password" type="password" show-password />
-				</el-form-item>
-			</div>
-		</el-form-item>
-	</el-form>
+    <el-form v-if="mounted" ref="form" :model="props.info" :rules="rule" label-width="auto" hide-required-asterisk
+        status-icon>
+        <el-form-item prop="ti" label="标题">
+            <el-input v-model="props.info.title" @change="$emit('change')" />
+        </el-form-item>
+        <el-form-item prop="ur" label="URL">
+            <el-input v-model="props.info.url" @change="$emit('change')" />
+        </el-form-item>
+        <el-form-item prop="tg" label="标签">
+            <el-select v-model="props.info.tag" multiple filterable remote collapse-tags collapse-tags-tooltip
+                :max-collapse-tags="5" remote-show-suffix :remote-method="fetchTags" :loading="tagsStatus === 'pending'"
+                @change="$emit('change')">
+                <el-option v-for="item in tagList" :label="item.ti || ''" :value="item._id" />
+                <template #loading>
+                    <svg class="circular" viewBox="0 0 50 50">
+                        <circle class="path" cx="25" cy="25" r="20" fill="none" />
+                    </svg>
+                </template>
+                <template #footer>
+                    <el-input v-model="newTagName" class="w-100p m-b-_5" placeholder="添加标签（暂不支持）" />
+                    <el-button type="primary" @click="confirmNewTag">
+                        添加
+                    </el-button>
+                    <el-button @click="newTagName = ''">
+                        取消
+                    </el-button>
+                </template>
+            </el-select>
+        </el-form-item>
+        <el-form-item prop="visible" label="可见性">
+            <div style="display: flex;">
+                <el-segmented v-if="props.info.visible !== null" v-model="props.info.visible" :options="visibleOptions">
+                    <template #default="{ item }">
+                        <div class="flex flex-col items-center gap-2 p-2">
+                            <div>{{ item.label }}</div>
+                        </div>
+                    </template>
+                </el-segmented>
+                <el-segmented v-else v-model="undef" :options="visibleOptions">
+                    <template #default="{ item }">
+                        <div class="flex flex-col items-center gap-2 p-2">
+                            <div>{{ item.label }}</div>
+                        </div>
+                    </template>
+                </el-segmented>
+                <el-form-item prop="pw" label="密码" v-if="props.info.visible === 2">
+                    <el-input v-model="props.info.password" type="password" show-password />
+                </el-form-item>
+            </div>
+        </el-form-item>
+    </el-form>
 </template>
 
 <style scoped></style>
