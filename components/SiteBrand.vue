95f328da7288f7c2f91a7bcf37e387ed902eef89
vvvbbbcz <ms_vvvbbbcz@mail.scut.edu.cn>
2026-03-07 22:51:01 +0800

    chore: default site name

--- components/SiteBrand.vue
+++ components/SiteBrand.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-const {data} = useNuxtData('blogInfo');
-const title = data.value ? data.value.name : '获取博客名称失败'
+const { data } = useNuxtData('blogInfo');
+const title = data.value?.name ?? 'NuxtBlog'
 </script>
 
 <template>
 	<span class="site-name">{{ title }}</span>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
