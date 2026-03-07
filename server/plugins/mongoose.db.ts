77adb2e31de89edd36c866ddf9ec73aaa360feb4
vvvbbbcz <ms_vvvbbbcz@mail.scut.edu.cn>
2026-03-07 23:31:42 +0800

    chore: format server plugin code

--- server/plugins/mongoose.db.ts
+++ server/plugins/mongoose.db.ts
@@ -3,33 +3,33 @@ import mongoose from "mongoose";
 import BlogData from "~/server/utils/models/BlogData";
 
 export default defineNitroPlugin(async (nitroApp) => {
-	mongoose.connection.once('open', () => {
-		consola.success('Connected to MongoDB');
-	});
+    mongoose.connection.once('open', () => {
+        consola.success('Connected to MongoDB');
+    });
 
-	mongoose.connection.once('close', () => {
-		consola.info('MongoDB disconnected');
-	});
+    mongoose.connection.once('close', () => {
+        consola.info('MongoDB disconnected');
+    });
 
-	nitroApp.hooks.hook('close', () => {
-		mongoose.disconnect();
-	});
+    nitroApp.hooks.hook('close', () => {
+        mongoose.disconnect();
+    });
 
-	if (!process.env.MONGODB_URI) {
-		consola.warn('No MONGODB_URI found in env');
-		return;
-	} else if (!process.env.MONGODB_DB_NAME) {
-		consola.warn('No MONGODB_DB_NAME found in env');
-		return;
-	}
+    if (!process.env.MONGODB_URI) {
+        consola.warn('No MONGODB_URI found in env');
+        return;
+    } else if (!process.env.MONGODB_DB_NAME) {
+        consola.warn('No MONGODB_DB_NAME found in env');
+        return;
+    }
 
-	const models = [BlogData];
-	consola.info(`Loaded ${models.length} Mongoose model(s)`);
+    const models = [BlogData];
+    consola.info(`Loaded ${models.length} Mongoose model(s)`);
 
-	try {
-		mongoose.set('strictQuery', true);
-		await mongoose.connect(process.env.MONGODB_URI, { dbName: process.env.MONGODB_DB_NAME });
-	} catch (error) {
-		consola.error(`Error connecting to MongoDB: ${error}`);
-	}
+    try {
+        mongoose.set('strictQuery', true);
+        await mongoose.connect(process.env.MONGODB_URI, { dbName: process.env.MONGODB_DB_NAME });
+    } catch (error) {
+        consola.error(`Error connecting to MongoDB: ${error}`);
+    }
 });
